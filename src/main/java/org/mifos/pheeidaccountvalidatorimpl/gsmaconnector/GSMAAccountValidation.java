ad78bb421f5ac80c9a50e5012f3880a39a7c1dd8
apurbraj <apurb@fynarfin.io>
2023-09-02 14:44:41 +0530

    GSMA Validation Changes

--- src/main/java/org/mifos/pheeidaccountvalidatorimpl/gsmaconnector/GSMAAccountValidation.java
+++ src/main/java/org/mifos/pheeidaccountvalidatorimpl/gsmaconnector/GSMAAccountValidation.java
@@ -14,9 +14,9 @@ import java.util.Map;
 
 @Service(value = "gsma")
 public class GSMAAccountValidation extends AccountValidationService {
-    @Value("${gsma-connector.contactpoint}")
+    @Value("${gsma_connector.contactpoint}")
     public String gsmaConnectorContactPoint;
-    @Value("${gsma-connector.endpoint.account-status}")
+    @Value("${gsma_connector.endpoint.account-status}")
     public String accountStatusEndpoint;
     @Override
     public Boolean validateAccount(String financialAddress, String tenant, String paymentModality, String payeeIdentity, String callbackURL){
@@ -35,22 +35,21 @@ public class GSMAAccountValidation extends AccountValidationService {
         String responseBody = response.then().extract().body().asString();
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> responseMap = null;
-        try {
-            responseMap = objectMapper.readValue(responseBody, Map.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        if(statusCode==200) {
+            try {
+                responseMap = objectMapper.readValue(responseBody, Map.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            String fieldValue = responseMap.get("fieldName").toString();
+
+            if (responseMap.get("accountStatus").equals("savingsAccountStatusType.active")) {
+                return true;
+            }
         }
 
-// Extract the value of the "fieldName" field
-        String fieldValue = responseMap.get("fieldName").toString();
-        if(!statusCode.equals(200)) {
-            return false;
-        } else if(statusCode.equals(200) && responseMap.get("accountStatus").equals("savingsAccountStatusType.active")){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return false;
+
     }
 }
 
